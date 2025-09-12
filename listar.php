3cf6be70b0494ce081a36d03bf1c734831fb9f14
Iago de Almeida Coelho <161508403+IagoEtec@users.noreply.github.com>
2025-09-12 15:44:38 -0300

    Add files via upload

--- listar.php
+++ listar.php
@@ -1,5 +1,6 @@
 <?php
-    include "cabecalho.php"
+    include "cabecalho.php";
+    require 'conexao.php'; // Move o require para antes de usar $pdo
 ?>
 
     <a href="index.php" class="back-button">← Voltar</a>
@@ -17,41 +18,34 @@
             </thead>
             <tbody>
 
-
-
-
             <?php
-                require 'conexao.php';
-                $sql = "SELECT * FROM produtos";
-                $stmt = $pdo->query($sql);
-                while ($produto = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                // Verifica se a conexão foi estabelecida
+                if ($pdo !== null) {
+                    $sql = "SELECT * FROM produtos";
+                    $stmt = $pdo->query($sql);
+                    while ($produto = $stmt->fetch(PDO::FETCH_ASSOC)) {
 
-                    echo "<tr>";
-                    echo "<td>" . $produto['id'] . "</td>";
-                    echo "<td>" . $produto['nome'] . "</td>";
-                    echo "<td>" . $produto['preco'] . "</td>";
-                    echo "<td>" . $produto['quantidade'] . "</td>";
-                    echo "
-                    <td>
-                        <div class='btn-group' role='group'>
-                            <a href='form_atualizado.php?id=" . $produto['id'] . "' type='button' class='btn btn-success'>Atualizar</a>
-                            <a href='#' type='button' class='btn btn-danger'>Apagar</a>
-                        </div>
-                    </td>";
-                    echo "</tr>";
+                        echo "<tr>";
+                        echo "<td>" . $produto['id'] . "</td>";
+                        echo "<td>" . $produto['nome'] . "</td>";
+                        echo "<td>" . $produto['preco'] . "</td>";
+                        echo "<td>" . $produto['quantidade'] . "</td>";
+                        echo "
+                        <td>
+                            <div class='btn-group' role='group'>
+                                <a href='form_atualizado.php?id=" . $produto['id'] . "' type='button' class='btn btn-success'>Atualizar</a>
+                                <a href='#' type='button' class='btn btn-danger'>Apagar</a>
+                            </div>
+                        </td>";
+                        echo "</tr>";
+                    }
+                } else {
+                    echo "<tr><td colspan='5'>Erro: Não foi possível conectar ao banco de dados</td></tr>";
                 }
             ?>
-
-    
 
             </tbody>
         </table>
     </div>
 </body>
 </html>
-
-<!-- 
-codigos BD
-SELECT * FROM produtos WHERE id = 1;
-UPDATE produtos SET nome = "NOVO NOME" WHERE id = 1; 
--->
